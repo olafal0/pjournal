2d826fc3e5ebec507230d4ce12f3315d6d0b1a64
Daniel Lawrence <lawrence.danielt@gmail.com>
2020-06-22 22:57:58 -0400

    Add password reset flow to login component

--- web/Login.svelte
+++ web/Login.svelte
@@ -1,36 +1,23 @@
 <script>
   import Auth from "./Auth";
   import { onMount, createEventDispatcher } from "svelte";
-  import Form from "./Form.svelte";
 
   const dispatch = createEventDispatcher();
 
   const FlowState = Object.freeze({
     loading: 1,
     initial: 2,
-    signUpConfirmation: 3
+    signUpConfirmation: 3,
+    forgotPassword: 4
   });
 
-  const initialFormFields = {
-    email: {
-      type: "email",
-      label: "Email Address"
-    },
-    password: {
-      type: "password",
-      label: "Password"
-    }
+  let formFields = {
+    email: "",
+    password: "",
+    newPassword: "",
+    confirmationCode: ""
   };
 
-  const signUpConfirmationFormFields = {
-    code: {
-      label: "Confirmation Code",
-      type: "text"
-    }
-  };
-
-  let mainForm = null;
-  let _username = "";
   let flowState = FlowState.loading;
   let infoMessage = "";
   let errorMessage = "";
@@ -57,29 +44,34 @@
     return JSON.stringify(msg);
   }
 
-  function displayError(msg) {
-    errorMessage = msgToString(msg);
+  function clearMessages() {
     infoMessage = "";
-  }
-
-  function displayInfo(msg) {
-    infoMessage = msgToString(msg);
     errorMessage = "";
   }
 
+  function displayError(msg) {
+    clearMessages();
+    errorMessage = msgToString(msg);
+  }
+
+  function displayInfo(msg) {
+    clearMessages();
+    infoMessage = msgToString(msg);
+  }
+
   function signedIn() {
-    console.log("dispatching signedIn");
     dispatch("signedIn");
   }
 
-  function signUp({ email, password }) {
+  function signUp() {
     displayInfo("Registering...");
-    _username = email;
+    // TODO: Special case handling for common errors or problems (eg "@gmail.con")
+    formFields.email = formFields.email;
     Auth.signUp({
-      username: email,
-      password,
+      username: formFields.email,
+      password: formFields.password,
       attributes: {
-        email
+        email: formFields.email
       }
     })
       .then(({ user, userConfirmed, userSub }) => {
@@ -97,13 +89,13 @@
   }
 
   function resendCode() {
-    displayInfo(`Resending confirmation code for ${_username}`);
-    Auth.resendSignUp(_username).catch(displayError);
+    displayInfo(`Resending confirmation code for ${formFields.email}`);
+    Auth.resendSignUp(formFields.email).catch(displayError);
   }
 
-  function confirmSignUp({ detail: data }) {
+  function confirmSignUp() {
     displayInfo("Confirming user...");
-    Auth.confirmSignUp(_username, data.code)
+    Auth.confirmSignUp(formFields.email, formFields.confirmationCode)
       .then(result => {
         if (result === "SUCCESS") {
           // Set back to initial, now that user needs to sign in for the first time
@@ -115,10 +107,9 @@
       .catch(displayError);
   }
 
-  function signIn({ detail: data }) {
+  function signIn() {
     displayInfo("Signing in...");
-    _username = data.email;
-    Auth.signIn(data.email, data.password)
+    Auth.signIn(formFields.email, formFields.password)
       .then(signedIn)
       .catch(err => {
         if (err.code === "UserNotConfirmedException") {
@@ -131,43 +122,151 @@
         }
       });
   }
+
+  async function startPasswordReset() {
+    if (!formFields.email) {
+      displayError("Please enter your email first");
+      return;
+    }
+    try {
+      await Auth.forgotPassword(formFields.email);
+      displayInfo(
+        "Please enter your new password and the confirmation code sent to your email address"
+      );
+      flowState = FlowState.forgotPassword;
+    } catch (e) {
+      displayError(e);
+    }
+  }
+
+  function cancelPasswordReset() {
+    clearMessages();
+    flowState = FlowState.initial;
+  }
+
+  async function confirmResetPassword() {
+    displayInfo("Resetting password...");
+    try {
+      await Auth.forgotPasswordSubmit(
+        formFields.email,
+        formFields.confirmationCode,
+        formFields.newPassword
+      );
+      displayInfo("Please log in with your new password");
+      flowState = FlowState.initial;
+    } catch (e) {
+      displayError(e);
+    }
+  }
 </script>
 
+<style>
+  .info-message {
+    margin: 10px;
+    padding: 5px;
+    border: 1px solid rgba(0, 195, 209, 0.5);
+    border-radius: 5px;
+    color: antiquewhite;
+  }
+
+  .error-message {
+    margin: 10px;
+    padding: 5px;
+    border: 1px solid rgba(209, 0, 0, 0.5);
+    border-radius: 5px;
+    color: rgb(250, 208, 153);
+  }
+</style>
+
 <div class="row">
-  <div class="col l6 offset-l3 m8 offset-m2 s12 center">
+  <div class="col l8 offset-l2 m10 offset-m1 s12 center">
     <div class="card grey darken-4">
+      {#if errorMessage}
+        <div class="col s12">
+          <div class="error-message">{errorMessage}</div>
+        </div>
+      {:else if infoMessage}
+        <div class="col s12">
+          <div class="info-message">{infoMessage}</div>
+        </div>
+      {/if}
       <div class="card-content white-text">
         <div class="row">
-          {#if errorMessage}
-            <div class="alert alert-danger">{errorMessage}</div>
-          {:else if infoMessage}
-            <div class="alert alert-primary">{infoMessage}</div>
-          {/if}
           <h5 class="card-title">Log in</h5>
           {#if flowState === FlowState.initial}
-            <Form
-              bind:this={mainForm}
-              formFields={initialFormFields}
-              on:submit={signIn}>
+            <form on:submit|preventDefault={signIn}>
+              <div class="form-group">
+                <label>Email Address</label>
+                <input
+                  type="email"
+                  class="white-text form-control"
+                  bind:value={formFields.email} />
+              </div>
+              <div class="form-group">
+                <label>Password</label>
+                <input
+                  type="button"
+                  class="btn btn-flat btn-small grey darken-4 grey-text"
+                  on:click={startPasswordReset}
+                  value="(Forgot password?)" />
+                <input
+                  type="password"
+                  class="white-text form-control"
+                  bind:value={formFields.password} />
+              </div>
               <input
                 type="button"
                 class="btn grey darken-2 left"
-                on:click={() => signUp(mainForm.getFormData())}
+                on:click={signUp}
                 value="Register" />
               <input type="submit" class="btn right" value="Log In" />
-            </Form>
+            </form>
           {:else if flowState === FlowState.signUpConfirmation}
-            <Form
-              bind:this={mainForm}
-              formFields={signUpConfirmationFormFields}
-              on:submit={confirmSignUp}>
+            <form on:submit|preventDefault={confirmSignUp}>
+              <div class="form-group">
+                <label>Confirmation Code</label>
+                <input
+                  type="text"
+                  class="white-text form-control"
+                  bind:value={formFields.confirmationCode} />
+              </div>
               <input
                 type="button"
                 class="btn grey darken-2 left"
-                on:click={() => resendCode(mainForm.getFormData())}
+                on:click={resendCode}
                 value="Resend Confirmation Code" />
               <input type="submit" class="btn right" value="Confirm" />
-            </Form>
+            </form>
+          {:else if flowState === FlowState.forgotPassword}
+            <form on:submit|preventDefault={confirmResetPassword}>
+              <div class="form-group">
+                <label>New Password</label>
+                <input
+                  type="password"
+                  class="white-text form-control"
+                  bind:value={formFields.newPassword} />
+              </div>
+              <div class="form-group">
+                <label>Confirmation Code</label>
+                <input
+                  type="text"
+                  class="white-text form-control"
+                  bind:value={formFields.confirmationCode} />
+              </div>
+              <div class="left">
+                <input
+                  type="button"
+                  class="btn grey darken-2"
+                  on:click={cancelPasswordReset}
+                  value="Cancel Reset" />
+                <input
+                  type="button"
+                  class="btn grey darken-2"
+                  on:click={startPasswordReset}
+                  value="Resend Code" />
+              </div>
+              <input type="submit" class="btn right" value="Confirm" />
+            </form>
           {/if}
         </div>
       </div>
