ae423e157caaf7e462ac9889e247efde07cb2b29
Daniel Lawrence <lawrence.danielt@gmail.com>
2020-06-27 12:10:43 -0400

    Add workbox, service worker caching, icons

--- web/index.js
+++ web/index.js
@@ -6,19 +6,3 @@ import 'regenerator-runtime/runtime';
 const app = new App({
   target: document.getElementById('svelte-app'),
 });
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/serviceWorker.js').then(
-      function (registration) {
-        console.log(
-          'ServiceWorker registration successful with scope: ',
-          registration.scope
-        );
-      },
-      function (err) {
-        console.log('ServiceWorker registration failed: ', err);
-      }
-    );
-  });
-}
